0c0064ff0be7f3f51159146e031aa09e12b7d1c4
Daniel K <daniel@192-168-1-161.tpgi.com.au>
2020-11-27 10:04:59 +1100

    update our menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import React from 'react';
 import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 
 function App() {
   return (
+    <Router>
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
+    </Router>
   );
 }
 
