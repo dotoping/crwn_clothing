da3ead2f932440ffb7b3615af31e59c13bacb607
Daniel K <daniel@192-168-1-161.tpgi.com.au>
2020-11-27 12:06:19 +1100

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
@@ -13,12 +14,14 @@ const HatsPage = () => (
 function App() {
   return (
     <Router>
-    <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
-      </Switch>
-    </div>
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/hats' component={HatsPage} />
+          <Route exact path='/shop' component={ShopPage} />
+
+        </Switch>
+      </div>
     </Router>
   );
 }
